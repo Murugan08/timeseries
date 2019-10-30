2ec75a2ca99e93686ec08c922394313bf3601deb
Murugan08 <56655314+Murugan08@users.noreply.github.com>
2019-10-30 16:24:28 -0500

    Update A3Q6.R

--- A3Q6.R
+++ A3Q6.R
@@ -1,4 +1,7 @@
-head(Mergedatafile)
+#Small project on Time series Analysis of bitcoin with respect to S&P500 (SP500), the London bullion market price
+#for gold in US dollars (GOLDPMGBD228NLBM), the US/Euro exchange rate (DEXUSEU), and the
+#West Texas Intermediate spot price of oil (DCOILWTICO).
+head(Mergedatafile)#merged the file, tried alteryx for a change.
 names(Mergedatafile)[2]<-"gold"
 names(Mergedatafile)[3]<-"bitcoin"
 names(Mergedatafile)[4]<-"oil"
@@ -8,7 +11,7 @@ head(Mergedatafile)
 library(ggplot2)
 Mergedatafile$Date <- as.Date(Mergedatafile$Date)
 plot(Mergedatafile)
-#(5)
+#(5)#Naive model without making the model stationary 
 model1 <- lm(gold~bitcoin+oil+exchange+SP500+Date, data=Mergedatafile)
 model2 <- lm(bitcoin~gold+oil+exchange+SP500+Date, data=Mergedatafile)
 model3 <- lm(exchange~bitcoin+oil+gold+SP500+Date, data=Mergedatafile)
@@ -17,7 +20,7 @@ summary(model1)
 summary(model2)
 summary(model3)
 summary(model4)
-#(6)
+#(6)#defining a function to get the outputs of KPSS in a better way.
 library(tseries)
 rep.kpss <- function(series){
   for(diffs in 0:5){
@@ -35,17 +38,17 @@ rep.kpss <- function(series){
 
 kpss.test(Mergedatafile$oil)
 kpss.test(Mergedatafile$SP500)
-rep.kpss(Mergedatafile$gold) #1 level
-rep.kpss(Mergedatafile$bitcoin) # 1 level
-rep.kpss(Mergedatafile$exchange) # 1 level  
-rep.kpss(Mergedatafile$SP500)# 1 level
-rep.kpss(Mergedatafile$oil) #1 level
+rep.kpss(Mergedatafile$gold) #1 level stationary 
+rep.kpss(Mergedatafile$bitcoin) # 1 level stationary
+rep.kpss(Mergedatafile$exchange) # 1 level stationary 
+rep.kpss(Mergedatafile$SP500)# 1 level stationary
+rep.kpss(Mergedatafile$oil) #1 level stationary
 #(7)
 model2 <- lm(diff(bitcoin)~diff(SP500)+diff(gold)+diff(exchange)+diff(oil), data = Mergedatafile)
 summary(model2)
 #Not a good model, we don't have much relationship between the variables
 
-#(8)
+#(8) Removed all the data before 2017 where the bitcoin price starts to spike.
 Newdata <- Mergedatafile [date>="2017-01-01"]
 
 Newdata <- Mergedatafile %>%
@@ -55,7 +58,7 @@ Newdata <- Mergedatafile %>%
 ts.plot(Newdata$bitcoin, gpars = list(xlab = "day" , ylab = "bitcoin"))
 ggplot(Newdata, aes(x=Date,bitcoin)) + geom_line()
 
-#(9)
+#(9) finding acf and pcf
 acf(diff(Newdata$bitcoin))
 pacf(diff(Newdata$bitcoin))
 
@@ -102,7 +105,7 @@ model5<- vars::VAR(mdata,p=1)
 summary(model5)
 #sp500 granger causes oil, oil and euro granger cause gold.
 
-#(15)
+#(15)Forecasting the next 30 days of the prices using the VAR model. 
 model7 <- Arima(Newdata$bitcoin,c(9,1,2))
 fcst <- predict(model5,n.ahead=38)$fcst$bitcoin[,1]
 last.value <- tail(Newdata[complete.cases(Newdata),fcst$bitcoin],1)
